22d0bd321c66015d701f14fe06f9dde5f29d2156
Crispin Quizo <cqm.cris@gmail.com>
2020-04-04 15:10:44 -0400

    first commit

---
+++ src/main/java/com/app/aforo255/seguridad/ServiceBankaforo255SeguridadApplication.java
@@ -0,0 +1,13 @@
+package com.app.aforo255.seguridad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBankaforo255SeguridadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBankaforo255SeguridadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/aforo255/seguridad/ServiceBankaforo255SeguridadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.aforo255.seguridad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBankaforo255SeguridadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
